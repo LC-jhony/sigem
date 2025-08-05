2d642db2202e94570323e3a9b6a322314d6ef910
LC-jhony <jhonyapm94@gmail.com>
2025-08-05 15:59:27 -0500

    updated proyect

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,12 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Indigo,
             ])
+            ->navigationGroups([
+                'Gestión de Personal',
+                'Gestión de Minas',
+                'Mantenimiento',
+                'Roles y Permisos',
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
