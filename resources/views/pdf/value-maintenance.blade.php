e920ee92ae1866cf21675aecbb5a89f6e5a63d3f
LC-jhony <jhonyapm94@gmail.com>
2025-08-15 16:14:25 -0500

    actualizacion pdf

--- resources/views/pdf/value-maintenance.blade.php
+++ resources/views/pdf/value-maintenance.blade.php
@@ -1,53 +1,50 @@
 <x-layout>
-    <div style="text-align: center; margin-bottom: 20px; margin-top: -60px;">
-        <h3>Valor de Mantenimiento del Vehículo</h3>
-    </div>
-    <div style="display: table; width: 100%;">
-        <!-- Columna izquierda: Datos del vehículo -->
-        <div
-            style="display: table-cell; border: 1px solid #e0e0e0; padding: 12px; border-radius: 6px; background-color: #fdfdfd; vertical-align: top;">
-            <div
-                style="font-weight: 600; font-size: 14px; margin-bottom: 6px; color: #111; border-bottom: 1px solid #ddd; padding-bottom: 4px;">
-                Vehículo
-            </div>
-            <div style="margin-bottom: 2px;"><strong>Placa:</strong> {{ $vehicle->placa }}</div>
-            <div style="margin-bottom: 2px;"><strong>Marca:</strong> {{ $vehicle->marca ?? '-' }}</div>
-            <div style="margin-bottom: 2px;"><strong>Unidad:</strong> {{ $vehicle->unidad ?? '-' }}</div>
-            <div style="margin-bottom: 2px;"><strong>Tarjeta Propiedad:</strong> {{ $vehicle->property_card ?? '-' }}
+    <x-slot name="head">
+        <div class="report-title">
+            <h1>VALORIZADO DE MANTENIMIENTO <br> VEHICULAR</h1>
+            <div class="report-number">
+                <strong style="font-widget: bold;"> Placa: </strong>
+                {{ $vehicle->placa }}<BR>
+                <strong style="font-widget: bold;"> Marca: </strong>
+                {{ $vehicle->marca ?? '-' }} <BR>
+                <strong style="font-widget: bold;"> Unidad: </strong>
+                {{ $vehicle->unidad ?? '-' }}< <BR>
+                <strong style="font-widget: bold;"> Tarjeta Pro.: </strong>
+                {{ $vehicle->property_card ?? '-' }} <BR>
+                {{-- <strong style="font-widget: bold;"> Mes: </strong>
+                mes --}}
             </div>
         </div>
-    </div>
-    <br>
-
-    <table width="100%">
-        <thead style="background-color: lightgray;">
-            <tr>
-                <th>Descripcion</th>
-                <th>KM</th>
-                <th>Precio Material</th>
-                <th>Mano de Obra</th>
-                <th>Costo Mantenimiento</th>
-            </tr>
+    </x-slot>
+    <table class="table">
+        <thead >
+        <tr>
+            <th>Descripcion</th>
+            <th>KM</th>
+            <th>Precio Material</th>
+            <th>Mano de Obra</th>
+            <th>Costo Mantenimiento</th>
+        </tr>
         </thead>
         <tbody>
-            @foreach ($records as $item)
-                <tr>
-                    <td>{{ $item->maintenanceItem->name }}</td>
-                    <td>{{ $item->mileage }}</td>
-                    <td style="text-align: right;">S/. {{ number_format($item->Price_material, 2) }}</td>
-                    <td style="text-align: right;">S/. {{ number_format($item->workforce, 2) }}</td>
-                    <td style="text-align: right;">S/. {{ number_format($item->maintenance_cost, 2) }}</td>
-                </tr>
-            @endforeach
+        @foreach ($records as $item)
+            <tr>
+                <td>{{ $item->maintenanceItem->name }}</td>
+                <td>{{ $item->mileage }}</td>
+                <td style="text-align: right;">S/. {{ number_format($item->Price_material, 2) }}</td>
+                <td style="text-align: right;">S/. {{ number_format($item->workforce, 2) }}</td>
+                <td style="text-align: right;">S/. {{ number_format($item->maintenance_cost, 2) }}</td>
+            </tr>
+        @endforeach
         </tbody>
         @php
             $total = $records->sum('maintenance_cost');
         @endphp
         <tfoot>
-            <tr>
-                <td colspan="4" style="text-align: right;"><strong>Total general:</strong></td>
-                <td style="text-align: right;"><strong>S/. {{ number_format($total, 2) }}</strong></td>
-            </tr>
+        <tr>
+            <td colspan="4" style="text-align: right; font-style: italic;"><strong>Total general:</strong></td>
+            <td style="text-align: right;"><strong>S/. {{ number_format($total, 2) }}</strong></td>
+        </tr>
         </tfoot>
     </table>
 
@@ -66,14 +63,14 @@
                 @if ($item->photo)
                     <div style="margin-bottom: 20px;">
                         <img src="{{ storage_path('app/public/' . $item->photo) }}"
-                            style="width: 90%; max-width: 500px; border: 1px solid #ccc; padding: 5px;">
+                             style="width: 90%; max-width: 500px; border: 1px solid #ccc; padding: 5px;">
                     </div>
                 @endif
 
                 @if ($item->file)
                     <div style="margin-bottom: 20px;">
                         <img src="{{ storage_path('app/public/' . $item->file) }}"
-                            style="width: 90%; max-width: 500px; border: 1px solid #ccc; padding: 5px;">
+                             style="width: 90%; max-width: 500px; border: 1px solid #ccc; padding: 5px;">
                     </div>
                 @endif
             @endforeach
